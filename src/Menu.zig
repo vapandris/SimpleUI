d53c6eb5b5ec4c4140c5e91055c60220187f4aef
vapandris <vapandris@gmail.com>
2024-05-24 17:42:43 +0200

    Started re-structuring code

---
+++ src/Menu.zig
@@ -0,0 +1,6 @@
+// One day all of this should be in Lua so I could easily modify it dynamiclly.
+const UI = @import(".UI.zig");
+
+pub const MainMenu = struct {};
+
+pub const PauseMenu = struct {};
